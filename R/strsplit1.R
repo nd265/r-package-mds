0376d19cb11644d4e2660499e33d2288a39fca38
nd265 <navyad265@gmail.com>
2022-01-17 16:46:53 -0800

    Add roxygen in strsplit1

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,4 +1,14 @@
-strsplit1 <- function(x, split){
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit(x, split = ",")
+strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
-
 }
